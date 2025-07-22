b5b252f4afd4b030c185a2f5b7db17779ba18245
haeyun0911 <haeyun0911@gmail.com>
2025-07-22 14:25:33 +0900

    충돌메세지 조건문안으로 이동
    필요없는 변수삭제

--- turtle02.py
+++ turtle02.py
@@ -38,27 +38,21 @@ obstacles=[(obs[0],obs[1],71)]
 def distance(x1,y1,x2,y2):
     return math.sqrt((x2-x1)**2+(y2-y1)**2)
 
-def check_collision(a,obs):
+def check_collision(a):
     robot_x,robot_y=a
     obs_x,obs_y=obs
     for obs_x,obs_y,radius in obstacles:
         collision_distance=distance(robot_x,robot_y,obs_x,obs_y)
         if collision_distance <= radius + 10:
-            print("충돌")
             return True
         
     return False
 while True:
     a=t.pos()
-    check_collision(a,obs)
+    if check_collision(a):
+        print("충돌")
+    else:
+        print("안전")
     t.fd(1)
 
 s.mainloop()
-'''
-t.fd(100)
-t.lt(45)
-t.fd(150)
-t.rt(90)
-t.fd(150)
-t.lt(45)
-'''
