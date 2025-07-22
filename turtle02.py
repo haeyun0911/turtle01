a83d9b1cfd8393e0fc9e98f741ceb117e3d05f1c
haeyun0911 <haeyun0911@gmail.com>
2025-07-22 12:10:55 +0900

    장애물 좌표

---
+++ turtle02.py
@@ -0,0 +1,51 @@
+import turtle
+import math
+#스크린 생성
+s=turtle.getscreen()
+
+#거북이 변수 지정
+t=turtle.Turtle()
+
+t.shape("turtle")
+a=t.pos()
+print(a)
+t.speed(1)
+#장애물 만들기
+t.penup()
+t.fd(50)
+t.pendown()
+t.rt(90)
+t.fd(50)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(50)
+#출발점으로이동
+t.penup()
+t.rt(90)
+t.fd(50)
+t.lt(45)
+t.fd(200)
+
+t.pendown()
+'''
+while True:
+    a=t.pos()
+    
+def check_collision(a):
+    robot_x,robot_y=a
+    
+    
+
+t.rt(180)
+t.fd(100)
+t.lt(45)
+t.fd(150)
+t.rt(90)
+t.fd(150)
+t.lt(45)
+'''
